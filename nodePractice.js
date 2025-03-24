7b49f8f865966fd7bbbfd6e72a1b56fe518ca500
tchristian13 <tchrstn4@gmail.com>
2025-03-23 19:10:22 -0500

    homew assign

---
+++ nodePractice.js
@@ -0,0 +1,19 @@
+// Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
+let firstName = "Tia"
+let lastName = "Williams"
+let fullName = getInput(1) + " "+ getInput(2);
+
+fullname.toUpperCase() +
+firstName.charCodeAt(0).toUpperCase()
+
+
+const getInput = require("./get-input.js");
+console.log('hello" cfullname')
+return getInput([1])
+node no_Practice.js firstName lastName
+
+
+
+
+
+
